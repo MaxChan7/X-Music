8273c8bbc3ad8d240ce8181d73c15e81b2100687
MaxChan7 <365293455@qq.com>
2017-10-12 01:12:17 +0800

    update actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,4 +5,5 @@ export const selectPlay = function({commit, state}, {list, index}) {
   commit(types.SET_PLAYLIST, list);
   commit(types.SET_CURRENT_INDEX, index);
   commit(types.SET_FULL_SCREEN, true);
+  commit(types.SET_PLAYING_STATE, true);
 }
